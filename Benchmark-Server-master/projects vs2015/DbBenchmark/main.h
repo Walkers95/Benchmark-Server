72f1bd430734965b67197ddccc5ed305df2fa8ef
PaulBenabou <paulbenabou@gmail.com>
2018-10-25 14:50:37 +0200

    Added SDL + Nuklear

--- Benchmark-Server-master/projects vs2015/DbBenchmark/main.h
+++ Benchmark-Server-master/projects vs2015/DbBenchmark/main.h
@@ -1,1 +1,29 @@
-#include <stdio.h>dazdazdzada
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <math.h>
+#include <limits.h>
+#include <time.h>
+
+#define WINDOW_WIDTH 1024
+#define WINDOW_HEIGHT 724
+
+#include <SDL.h>
+#include <SDL_opengl.h>
+
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_IMPLEMENTATION
+#define NK_SDL_GL2_IMPLEMENTATION
+#include "Library\nuklear.h"
+
+#include "Library\nuklear_sdl_gl2.h"
